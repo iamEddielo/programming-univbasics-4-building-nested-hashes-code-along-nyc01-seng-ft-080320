91e4ccfdcd23e75218523863f7a9f00b111c3b00
Eddie Lopez <iameddielopez@gmail.com>
2020-07-17 19:01:08 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -13,9 +13,9 @@ def third_challenge
         #your key/value pairs here
         :name => "Romeo" , :age => "15", :status => "alive"
         },
-      :hero_friends =>[
-        {name:"Benvolio", age:"17", attitude:"worried"},
-        {name:"Mercutio", age:"18", attitude:"hot-headed"}
+      :hero_friends => [
+        {:name =>"Benvolio", :age => "17", :attitud =>"worried"},
+        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
         ]
    }, 
    :capulet => {
