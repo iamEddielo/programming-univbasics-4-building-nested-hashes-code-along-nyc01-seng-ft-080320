b8392f8884a1dfece7bfe37d639be5f802855316
Eddie Lopez <iameddielopez@gmail.com>
2020-07-17 19:02:41 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
         :name => "Romeo" , :age => "15", :status => "alive"
         },
       :hero_friends => [
-        {:name =>"Benvolio", :age => "17", :attitud =>"worried"},
+        {:name =>"Benvolio", :age => "17", :attitude =>"worried"},
         {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
         ]
    }, 
